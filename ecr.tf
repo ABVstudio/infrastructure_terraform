622343df2e508dbc41ad895b27047570d0a82f64
ABVstudio <gitlab@STOXYEB.com>
2025-11-19 16:02:38 +0300

    Update ecr.tf
    IMMUTABLE_WITH_EXCLUSION

--- ecr.tf
+++ ecr.tf
@@ -3,16 +3,18 @@ module "ecr" {
 
   repository_name = var.repository_name
   repository_read_write_access_arns = [ var.repository_iam_full ]
+  repository_image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+  repository_image_tag_mutability_exclusion_filter = ['latest']
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 30 images",
+        description  = "Keep last 3 images",
         selection = {
           tagStatus     = "tagged",
           tagPrefixList = ["v"],
           countType     = "imageCountMoreThan",
-          countNumber   = 30
+          countNumber   = 3
         },
         action = {
           type = "expire"
